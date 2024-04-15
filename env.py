c4d9fc82153598f3a12b3d84c493ab1b9831b970
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-15 22:56:10 +0200

    Setup project and add profile model

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ['CLOUDINARY_URL'] = 'cloudinary://353147318672354:80TIOaudrBGGDmhgO5IoWVE1SMk@dxr4xewpz'
+os.environ['SECRET_KEY'] = 'django-insecure-^*$laxxvsgvd1n5qu9%nla832l@kwj@12@2+cb9-fc$l6q5gih'
